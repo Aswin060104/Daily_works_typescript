3fc0364d56f5140f2bc98878c018bc1257759cab
Aswin060104 <trajaaswin@gmail.com>
2024-09-27 15:18:57 +0530

    static properties as arrays

--- day6/static_method.js
+++ day6/static_method.js
@@ -1,5 +1,6 @@
 var company = /** @class */ (function () {
     function company(name) {
+        this.employeeName = name;
         company._name[company.employeeCount] = name;
         company._id[company.employeeCount] = company.employeeCount;
         company.employeeCount++;
@@ -10,8 +11,11 @@ var company = /** @class */ (function () {
     company.prototype.getEmployeeName = function (_id) {
         return company._name[_id - 1];
     };
-    company.prototype.getAllEmployee = function () {
+    company.getAllEmployee = function () {
         return company._name;
+    };
+    company.prototype.getName = function () {
+        return this.employeeName;
     };
     company.employeeCount = 0;
     company._name = [];
@@ -19,9 +23,10 @@ var company = /** @class */ (function () {
     return company;
 }());
 var c = new company("Aswin");
-var c = new company("Arul");
+var c1 = new company("Arul");
 var c = new company("Sugu");
 var c = new company("Rahul");
+console.log(c.getName());
+console.log(c1.getName());
 console.log(c.getEmployeeCount());
-console.log(c.getEmployeeName(1));
-console.log(c.getAllEmployee().join(" "));
+console.log(company.getAllEmployee().join(" "));
--- day6/static_method.ts
+++ day6/static_method.ts
@@ -1,10 +1,13 @@
 class company{
     private static employeeCount : number = 0;
 
+    employeeName : string;
+
     static _name : string[] = [];
     static _id : number[] = [];
 
     constructor( name : string){
+        this.employeeName = name;
         company._name[company.employeeCount] = name;
         company._id[company.employeeCount] = company.employeeCount;
 
@@ -22,14 +25,18 @@ class company{
     static getAllEmployee() : string[]{
         return company._name;
     }
+
+    getName(){
+        return this.employeeName;
+    }
 }
 
 var c = new company("Aswin");
-var c = new company("Arul");
+var c1 = new company("Arul");
 var c = new company("Sugu");
 var c = new company("Rahul");
 
+console.log(c.getName());
+console.log(c1.getName());
 console.log(c.getEmployeeCount());
-console.log(c.getEmployeeName(1));
 console.log(company.getAllEmployee().join(" "));
-
