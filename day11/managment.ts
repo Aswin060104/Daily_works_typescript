8cf5a2bc76b379bfef3ab77d3eabf429a4a7f691
Aswin060104 <trajaaswin@gmail.com>
2024-10-01 11:04:32 +0530

    clg managment

---
+++ day11/managment.ts
@@ -0,0 +1,26 @@
+interface College{
+    collegeName : string;
+    studentCount : number;
+    getCollegeDetails() : string;
+}
+
+interface Deparment{
+    departmentName : string;
+    departmentStudentCount : number;
+    getDepartmentDetails() : string;
+}
+
+class kcet implements College{
+    collegeName: string = "Kcet";
+    studentCount: number = 1200;
+    getCollegeDetails(): string {
+        return "College Name : "+ this.collegeName + "\nStudent Count : "+this.studentCount;
+    }
+}
+class cse extends kcet implements Deparment{
+    departmentName: string = "CSE";
+    departmentStudentCount: number = 126;
+    getDepartmentDetails(): string {
+        return "Department Name : " + this.departmentName +"\nStudents Count : "+this.departmentStudentCount;
+    }
+}
